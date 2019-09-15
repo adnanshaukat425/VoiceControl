330d70b9c4cd6ff2292fa3c91e4472983e0c594e
Adnan Shaukat <adnanshaukat425@gmail.com>
2019-09-15 16:04:27 +0500

    Add project files.

---
+++ VoiceControl/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace VoiceControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 11.25F, System.Drawing.FontStyle.Bold);
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedHorizontal;
+            this.richTextBox1.Size = new System.Drawing.Size(721, 304);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 332);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ VoiceControl/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace VoiceControl
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine speechRecongnitionEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+        string[] commands_array = new string[] { "open google chrome", "open chrome", "exit google chrome", "Good bye", "bye bye", "Exit your self", "Stop listening", "Stop listening to me", "hi how are you"};
+
+        Dictionary<string, bool> recognized = new Dictionary<string, bool>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(commands_array);
+            GrammarBuilder grammarBuilder = new GrammarBuilder();
+            grammarBuilder.Append(commands);
+            grammarBuilder.Culture = CultureInfo.CurrentCulture;
+            string grammerPhrase = grammarBuilder.DebugShowPhrases;
+
+            Grammar grammar = new Grammar(grammarBuilder);
+            speechRecongnitionEngine.LoadGrammarAsync(grammar);
+            speechRecongnitionEngine.SetInputToDefaultAudioDevice();
+
+            speechRecongnitionEngine.SpeechRecognized += SpeechRecongnitionEngine_SpeechRecognized;
+            speechRecongnitionEngine.SpeechRecognitionRejected += SpeechRecongnitionEngine_SpeechRecognitionRejected;
+            speechRecongnitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        private void SpeechRecongnitionEngine_SpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        { 
+            float confidence = e.Result.Confidence;
+            string command = e.Result.Text.ToLower().Trim();
+            if (String.IsNullOrEmpty(command))
+            {
+                return;
+            }
+            if (recognized.ContainsKey(command))
+            {
+                return;
+            }
+            if (confidence < 0.5)
+            {
+                return;
+            }
+            switch (command)
+            {
+                case "hi how are you":
+                    speechSynthesizer.SpeakAsync("I am fine, how are you ?");
+                    recognized.Add(command, true);
+                    break;
+                case "open google chrome":
+                case "open chrome":
+                    speechSynthesizer.SpeakAsync("Opening Chrome");
+                    Process.Start("chrome.exe", "http://www.google.com");
+                    recognized.Add(command, true);
+                    break;
+                case "exit google chrome":
+                case "exit chrome":
+                    speechSynthesizer.SpeakAsync("Exiting Chrome!");
+                    recognized.Add(command, true);
+                    break;
+                case "good bye":
+                case "bye bye":
+                case "exit your self":
+                    speechSynthesizer.Speak("Exiting now");
+                    Environment.Exit(0);
+                    recognized.Add(command, true);
+                    break;
+                case "stop listening":
+                case "stop listening to me":
+                    speechSynthesizer.Speak("Okay I will not listen to you now");
+                    speechRecongnitionEngine.RecognizeAsyncStop();
+                    recognized.Add(command, true);
+                    break;
+                default:
+                    //MessageBox.Show("Command unrecognized, Please try again!");
+                    break;
+            }
+            recognized.Remove(command);
+            writeLog("<command> " + e.Result.Text + " <unrecognized> <confidence> " + confidence + " <" + DateTime.Now.ToString("yyyy_MM_dd HH:mm:ss") + ">");
+            //MessageBox.Show("Command unrecognized, Please try again! " + e.Result.Text + ".");
+        }
+
+        private void SpeechRecongnitionEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            float confidence = e.Result.Confidence;
+            string command = e.Result.Text.ToLower().Trim();
+            if (String.IsNullOrEmpty(command))
+            {
+                return;
+            }
+            if (recognized.ContainsKey(command))
+            {
+                return;
+            }
+            if (confidence < 0.5)
+            {
+                return;
+            }
+            switch (command)
+            {
+                case "hi how are you":
+                    speechSynthesizer.SpeakAsync("I am fine, how are you ?");
+                    recognized.Add(command, true);
+                    break;
+                case "open google chrome":
+                case "open chrome":
+                    speechSynthesizer.SpeakAsync("Opening Chrome");
+                    Process.Start("chrome.exe", "http://www.google.com");
+                    recognized.Add(command, true);
+                    break;
+                case "exit google chrome":
+                case "exit chrome":
+                    Process[] chromeInstances = Process.GetProcessesByName("chrome");
+                    chromeInstances[0].Close();
+                    speechSynthesizer.SpeakAsync("Exiting Chrome!");
+                    recognized.Add(command, true);
+                    break;
+                case "good bye":
+                case "bye bye":
+                case "exit your self":
+                    speechSynthesizer.Speak("Exiting now");
+                    Environment.Exit(0);
+                    recognized.Add(command, true);
+                    break;
+                case "stop listening":
+                case "stop listening to me":
+                    speechSynthesizer.Speak("Okay I will not listen to you now");
+                    speechRecongnitionEngine.RecognizeAsyncStop();
+                    recognized.Add(command, true);
+                    break;
+                default:
+                    //MessageBox.Show("Command unrecognized, Please try again!");
+                    break;
+            }
+            recognized.Remove(command);
+            writeLog("<command> " + e.Result.Text + " <recognized> <confidence> " + confidence + " <" + DateTime.Now.ToString("yyyy_MM_dd HH:mm:ss") + ">");
+        }
+
+        public void writeLog(string log)
+        {
+            richTextBox1.AppendText(log + Environment.NewLine);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
